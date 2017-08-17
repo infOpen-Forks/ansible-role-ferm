67af19aba788fa5d0f0afbe3861726c7a82baa4a
Alexandre Chaussier <alexandre.chaussier@temelio.com>
2017-08-17 09:10:52 +0200

    Remove legacy files and replace serverspec by testinfra

--- tests/test_installation.py
+++ tests/test_installation.py
@@ -2,14 +2,59 @@
 Role tests
 """
 
+import pytest
 from testinfra.utils.ansible_runner import AnsibleRunner
 
 testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
-def test_foo_a(User):
-    assert User().name == 'root'
+@pytest.mark.parametrize('name', [
+    ('ferm'),
+    ('iptables'),
+])
+def test_packages(host, name):
+    """
+    Test packages installed
+    """
+
+    assert host.package(name).is_installed
 
 
-def test_foo_b(User):
-    assert User().name == 'root'
+@pytest.mark.parametrize('item_type,path,user,group,mode', [
+    ('file', '/etc/ferm/ferm.conf', 'root', 'root', 0o400),
+])
+def test_files_and_folders(host, item_type,path,user,group,mode):
+    """
+    Test files and folders properties
+    """
+
+    current_item = host.file(path)
+
+    if item_type == 'directry':
+        assert current_item.is_directory
+    elif item_type == 'file':
+        assert current_item.is_file
+
+    assert current_item.user == user
+    assert current_item.group == group
+    assert current_item.moe == mode
+
+
+def test_firewall_rules(host):
+    """
+    Test firewall rules
+    """
+
+    expected_rules = [
+        '-P INPUT ACCEPT',
+        '-P FORWARD DROP',
+        '-P OUTPUT ACCEPT',
+        '-A INPUT -m state --state INVALID -j DROP',
+        '-A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT',
+        '-A INPUT -i lo -j ACCEPT',
+        '-A INPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT',
+        '-A INPUT -p tcp -m tcp --dport 22 -j ACCEPT',
+        '-A INPUT -j DROP',
+    ]
+
+    assert host.check_output('iptables -S').split("\n") == expected_rules
